09e3eafb728c95aed5130f144a5de4e8b80816d3
Kaio Henrique <96930584+KaioHSG@users.noreply.github.com>
2022-11-22 10:44:49 -0300

    Delete Matemática e String directory

--- Matemática e String/src/matematica/e/string/Matematica.java
+++
@@ -1,7 +0,0 @@
-package matematica.e.string;
-public class Matematica {
-    public static void main(String args[]) {
-        double x = Math.sqrt(25), y = Math.PI, z = Math.sin(4);
-        System.out.println("Raiz = " + x + "\nSeno = " + z + "\nPI = " + y);
-    }
-}
