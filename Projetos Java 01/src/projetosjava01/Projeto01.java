2996985ae2521c0d80666236b00f1d13da9f6ecb
Kaio Henrique <96930584+KaioHSG@users.noreply.github.com>
2022-11-22 10:54:36 -0300

    Add files via upload

---
+++ Projetos Java 01/src/projetosjava01/Projeto01.java
@@ -0,0 +1,14 @@
+package projetosjava01;
+public class Projeto01 {
+    public static void main(String args[]) {
+        double salario = 1500.30;
+        int inteiro;
+        inteiro = (int)salario;
+        System.out.println(inteiro);
+        inteiro = (int)2.8;
+        System.out.println(inteiro);
+        inteiro = 200;
+        byte x = (byte)inteiro;
+        System.out.println(x);
+    }
+}
