4fffa8ad586af3ff8a7ab48044ad6db8477a508d
Kaio Henrique <96930584+KaioHSG@users.noreply.github.com>
2022-11-22 10:55:20 -0300

    Add files via upload

---
+++ Projetos Java 02/src/projetosjava02/Projeto02.java
@@ -0,0 +1,18 @@
+package projetosjava02;
+public class Projeto02 {
+    public static void main(String args[]) {
+        int i = 0;
+        
+//        while (i <= 10) {
+//            System.out.print(i + " ");
+//            i++;
+//        }
+        
+        do {
+            System.out.print(i + " ");
+            i++; 
+        } while (i <= 10);
+        
+        System.out.println("\nAacbou.");
+    }
+}
